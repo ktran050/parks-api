fc0dfaad03295e3361a672f356b80ac4b264e5ff
Kevin Tran <ktran050@gmail.com>
2020-04-01 16:13:06 -0700

    fetched successfully, TODO: fetch within search params, user feedback to wait for fetch

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 let toSearchList = []; // holds states users want to add *Note: statesList li elements are added independently
-let apiKey = "DytqKSWlz71A3zpgAJl8m0NxUDMko4SlH5Jmv6Jd";
+const options = {
+  headers: new Headers({
+    "X-Api-Key": "DytqKSWlz71A3zpgAJl8m0NxUDMko4SlH5Jmv6Jd"
+  })
+};
+
+// https://developer.nps.gov/api/v1/parks?api_key=DytqKSWlz71A3zpgAJl8m0NxUDMko4SlH5Jmv6Jd
 
 function fetchParks() {
-  fetch("developer.nps.gov/api/v1");
+  fetch(
+    "https://developer.nps.gov/api/v1/parks?api_key=DytqKSWlz71A3zpgAJl8m0NxUDMko4SlH5Jmv6Jd"
+  )
+    .then(function(result) {
+      if (result.ok) {
+        return result.text();
+      } else {
+        throw new Error("fetch failed");
+      }
+    })
+    .then(function(result) {
+      console.log(result);
+    });
+  console.log("here");
 }
 
 function handleAddState() {
